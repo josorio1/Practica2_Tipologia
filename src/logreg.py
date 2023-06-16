05c71a6132359ee6cc4137fb18ea19bb95e066fe
Jacobo Osorio <jacoboosorio1d@gmail.com>
2023-06-16 14:55:12 +0200

    .

--- src/logreg.py
+++ src/logreg.py
@@ -12,22 +12,22 @@ wine_data = pd.read_csv("data/winequality_combinado.csv")
 
 # Variables dependientes
 cols = [col for col in wine_data.columns if col != 'type']
-best_aic = 10e12  # Initialize with a large value
+best_aic = 10e12  # Inicializar aic
 best_model = None
 
 # División entre conjunto de entrenamiento y test
-x_data = wine_data[cols]  # Replace "target_column_name" with the actual name of the target column
-y_data = wine_data["type"]  # Replace "target_column_name" with the actual name of the target column
+x_data = wine_data[cols]  
+y_data = wine_data["type"]  
 
 # Perform train-test split
 X_train, X_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.2, random_state=42)
 
 
-# Iterate through all possible combinations of features and select the model with the lowest AIC
+# Buscar modelo con menor AIC
 for k in range(1, len(cols) + 1):
     for subset in set(itertools.combinations(cols, k)):
         X = X_train[list(subset)]
-        X = sm.add_constant(X)  # Add constant term for intercept
+        X = sm.add_constant(X) 
         y = y_train
         model = sm.Logit(y, X)
         result = model.fit()
@@ -45,7 +45,7 @@ X_test_best = sm.add_constant(X_test[list(best_subset)])
 predictions = best_model.predict(X_test_best)
 
 # Sacar métricas
-threshold = 0.5  # Adjust the threshold as per your needs
+threshold = 0.5 
 y_pred = np.where(predictions >= threshold, 1, 0)
 
 # Calcular métricas
